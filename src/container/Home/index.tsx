34d0a6c37d0c43bb3b7101c1c9f221d6aad19209
vini <viniciusnunes1709@gmail.com>
2022-03-29 10:59:32 -0300

    refactor: prettier

--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -9,26 +9,25 @@ import Header from '@/components/Header'
 export default function Home() {
     const [viewState, setViewState] = useState<ViewState>(null)
 
-
-    return <Container>
-        <Header/>
-        <Map
-            {...viewState}
-            onMove={event => setViewState(event.viewState)}
-            initialViewState={{
-                longitude: -46.729814074270706,
-                latitude: -23.562668693769865,
-                zoom: 14.5
-            }}
-            style={{width: '100%', height: '100%'}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-            mapboxAccessToken={env.mapboxToken}
-            maxBounds={[
-                [-46.779078543110245, -23.58418234374612],
-                [-46.6607033378379, -23.535484034820612]
+    return (
+        <Container>
+            <Header />
+            <Map
+                {...viewState}
+                onMove={event => setViewState(event.viewState)}
+                initialViewState={{
+                    longitude: -46.729814074270706,
+                    latitude: -23.562668693769865,
+                    zoom: 14.5
+                }}
+                style={{ width: '100%', height: '100%' }}
+                mapStyle='mapbox://styles/mapbox/streets-v9'
+                mapboxAccessToken={env.mapboxToken}
+                maxBounds={[
+                    [-46.779078543110245, -23.58418234374612],
+                    [-46.6607033378379, -23.535484034820612]
                 ]}
-            onClick={(event) => console.log(event.lngLat) }
-        >
-        </Map>
-    </Container>
+                onClick={event => console.log(event.lngLat)}></Map>
+        </Container>
+    )
 }
