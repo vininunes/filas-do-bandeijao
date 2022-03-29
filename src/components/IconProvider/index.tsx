7ad4842f075dce6b74d79bfbfab2242eb523bf68
vini <viniciusnunes1709@gmail.com>
2022-03-29 10:58:56 -0300

    feat: add icons

--- src/components/IconProvider/index.tsx
+++ src/components/IconProvider/index.tsx
@@ -1,14 +1,15 @@
-import { ComponentProps } from "./types";
+import { ComponentProps } from './types'
 
-import {
-    ExpandMore
-} from '@material-ui/icons'
-
+import { ExpandMore, Place, Speed } from '@material-ui/icons'
 
 export default function IconProvider({ icon, size }: ComponentProps) {
     switch (icon) {
         case 'expandMore':
             return <ExpandMore fontSize={size} />
+        case 'place':
+            return <Place fontSize={size} />
+        case 'speed':
+            return <Speed fontSize={size} />
         default:
             return <div />
     }
